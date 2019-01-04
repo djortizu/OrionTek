a7838b0c8a2e8c7e51c75580aacf0783d341cf0f
Dagoberto J. Ortiz <djortizu@outlook.com>
2019-01-04 07:06:38 -0400

    Add project files.

---
+++ ERCSelenium/Tools/Assertions.cs
@@ -0,0 +1,27 @@
+﻿using ERCSelenium.SUT;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERCSelenium.Tools
+{
+    public class Assertions
+    {
+        public static void AreEqual(string a, string b, string message)
+        {
+            try
+            {
+                Assert.AreEqual(a, b, message);
+                App.Reporter.ReportAssertion("element", "AreEqual", a, b, true, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                App.Reporter.ReportAssertion("element", "AreEqual", a, b, false, ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ ERCSelenium/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static Selenio.Core.SUT.SUTDriver;
+using ERCSelenium.SUT;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using ERCSelenium.Tools;
+
+namespace ERCSelenium
+{
+    [TestClass]
+    public class ERCSeleniumTest : MasterTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RunTest(() =>
+            {
+                Reporter.TestDescription = "Navigate to Google and make a search.";
+                App.OpenURL();
+                App.Google.Search("Youtube");
+            });
+        }
+
+        [TestMethod]
+        public void AutoConfigFileExists()
+        {
+            RunTest(() =>
+            {
+                Reporter.TestDescription = "Read my XML.";
+                Reporter.TestStep = "Get deployment directory";
+                Assertions.AreEqual(AutoConfig.XmlFilePath, AutoConfig.XmlFilePath, "File paths are not the same.");
+                Assertions.AreEqual("", AutoConfig.XmlFilePath, "File paths are not the same.");
+
+            });
+        }
+        
+    }
+}
